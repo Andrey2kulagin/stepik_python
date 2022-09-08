61e4d1989b215c8c891a94a70afc3b9144e37990
Андрей Кулагин <72682155+Andrey2kulagin@users.noreply.github.com>
2022-09-08 13:18:00 +0300

    Create 10.py

---
+++ day2/10.py
@@ -0,0 +1,12 @@
+timur_choice = input()
+ruslan_choice = input()
+if timur_choice == ruslan_choice:
+    print("ничья")
+else:
+    result_str = timur_choice+ruslan_choice
+    if result_str in ("ножницыбумага","бумагакамень","каменьножницы",
+"ножницыящерица","бумагаСпок","каменьящерица","ящерицабумага","ящерицаСпок"
+,"Спокножницы","Споккамень"):
+        print("Тимур")
+    else:
+        print("Руслан")
