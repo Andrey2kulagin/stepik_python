7a297eafb74a02164703a4be5d0007ea37712f23
Андрей Кулагин <72682155+Andrey2kulagin@users.noreply.github.com>
2022-09-16 14:32:33 +0300

    Update 5.py

--- day8/5.py
+++ day8/5.py
@@ -6,3 +6,6 @@ def print_products(*args):
             count+=1
     if count==1:
         print("Нет продуктов")
+def info_kwargs(**kwargs):
+    for i in sorted(kwargs):
+        print(f"{i}: {kwargs[i]}")
